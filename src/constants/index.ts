8afbd8d01b13f73de9aa3f883bf4c5aa119673df
hankliu <397694072@qq.com>
2024-05-24 09:43:51 +0800

    feat(questions): add hooks and utils

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+export const PageTitle = 'H.L Frontend - 前端面试题';
+
+export const PageDescription = `欢迎来到我们的前端面试题网站，这里每一道题目都精心设计，帮助您提升前端开发技能。在这里，您不仅能练习和提高技术，还能感受到编程的乐趣与创意。不论您是新手还是资深开发者，我们都希望这个平台能为您的学习之路增添一份美好与灵感。`;
+
+export const PageKeywords = [
+  'frontend interview',
+  'interview',
+  '前端面试',
+  '前端面试题',
+  'toolkit',
+  'toolkits',
+  '前端开发',
+  '前端开发工具',
+  '前端开发工具集合',
+  '在线工具',
+  'toolbox',
+  'frontend',
+  '卡鲁秋',
+  'Hank',
+  'HankLiu',
+];
