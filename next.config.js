2b4cb45d6eaf30eb16228b1404cff2619f6ba9da
hankliu <397694072@qq.com>
2026-03-06 22:19:47 +0800

    feat(build): refine next config file

--- next.config.js
+++ next.config.js
@@ -32,14 +32,14 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 if (isGithubActions) {
   nextConfig.output = 'export'; // 启用静态导出，输出到 out 目录
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  const repo = (process.env.GITHUB_REPOSITORY || '').replace(/.*?\//, '') || 'questions';
   // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
   // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
-  nextConfig.assetPrefix = `/${repo}/`;
+  nextConfig.assetPrefix = repo ? `/${repo}/` : '/';
   // 用于为应用设置基础路径(Link组件中，类似 history 里面的basename，在路由跳转时自动加前缀)
   // 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
-  nextConfig.basePath = `/${repo}`;
-  nextConfig.env.ROUTE_PREFIX = `/${repo}`;
+  nextConfig.basePath = repo ? `/${repo}` : '';
+  nextConfig.env.ROUTE_PREFIX = repo ? `/${repo}` : '';
 
   const {
     env: { NEXT_PUBLIC_GITHUB_FRONTEND_TOKEN, ...envs },
